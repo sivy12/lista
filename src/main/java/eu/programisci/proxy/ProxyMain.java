7300c68d824504a4ef5a95842d9e505ad623af58
jakub <jakub.kolecki@programisci.eu>
2017-05-10 19:45:32 +0200

    podbicie wersji spring cloud netflix -test

--- src/main/java/eu/programisci/proxy/ProxyMain.java
+++ src/main/java/eu/programisci/proxy/ProxyMain.java
@@ -2,10 +2,16 @@ package eu.programisci.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@EnableZuulProxy
+@EnableZuulServer
+@SpringBootApplication()
+@EnableCaching
+@EnableScheduling
+@EnableDiscoveryClient
 public class ProxyMain {
 
   public static void main(String[] args) {
